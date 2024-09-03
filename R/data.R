7f2c331aff36d1d59c1c2e4000859378e2327dee
robinlovelace-ate <robin.lovelace@dft.gov.uk>
2024-09-03 10:50:41 +0100

    Add pkg website

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Example zones dataset: administrative zones of York
+#' 
+#' See data-raw/zones_york.qmd for details on the data source.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name zones_york
+#' @examples
+#' head(zones_york)
+#' sf:::plot.sfg(zones_york$geometry)
+NULL
+
+#' Example destinations dataset: schools in York
+#' 
+#' Example dataset from York, UK
+#' 
+#' See data-raw/zones_york.qmd for details on the data source.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name destinations_york
+#' @examples
+#' head(destinations_york)
+NULL
